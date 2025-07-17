2a40832f8a9d31ecb733dd812e0f96425f23f083
Khaliq <bias.khaliq@gmail.com>
2025-07-17 09:05:01 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,7 +2,7 @@
 
 Terminal Colors
 
-GREEN="\033[0;32m" RED="\033[0;31m" YELLOW="\033[1;33m" RESET="\033[0m"
+GREEN="\033[0;32m" RED="\033[0;31m" YELLOW="\033[1;33m" CYAN="\033[0;36m" RESET="\033[0m"
 
 trap "echo; echo '[ABORTED] Process cancelled by user'; exit 1" INT
 
